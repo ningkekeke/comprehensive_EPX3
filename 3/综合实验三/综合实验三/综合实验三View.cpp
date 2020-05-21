bb7470d25e000258a598783c0825091254ee4e31
ningkekeke <2290418168@qq.com>
2020-05-21 22:01:37 +0800

    3

---
+++ 3/综合实验三/综合实验三/综合实验三View.cpp
@@ -0,0 +1,143 @@
+
+// 综合实验三View.cpp : C综合实验三View 类的实现
+//
+
+#include "stdafx.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "综合实验三.h"
+#endif
+
+#include "综合实验三Set.h"
+#include "综合实验三Doc.h"
+#include "综合实验三View.h"
+#include "ADD_DLG.h"
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// C综合实验三View
+
+IMPLEMENT_DYNCREATE(C综合实验三View, CRecordView)
+
+BEGIN_MESSAGE_MAP(C综合实验三View, CRecordView)
+	ON_BN_CLICKED(IDC_BUTTON3, &C综合实验三View::OnBnClickedButton3)
+	ON_BN_CLICKED(IDC_BUTTON1, &C综合实验三View::OnBnClickedButton1)
+	ON_BN_CLICKED(IDC_BUTTON2, &C综合实验三View::OnBnClickedButton2)
+END_MESSAGE_MAP()
+
+// C综合实验三View 构造/析构
+
+C综合实验三View::C综合实验三View()
+	: CRecordView(IDD_MY_FORM)
+	, l(0)
+{
+	m_pSet = NULL;
+	// TODO: 在此处添加构造代码
+
+}
+
+C综合实验三View::~C综合实验三View()
+{
+}
+
+void C综合实验三View::DoDataExchange(CDataExchange* pDX)
+{
+	CRecordView::DoDataExchange(pDX);
+	// 可以在此处插入 DDX_Field* 函数以将控件“连接”到数据库字段，例如
+	// DDX_FieldText(pDX, IDC_MYEDITBOX, m_pSet->m_szColumn1, m_pSet);
+	// DDX_FieldCheck(pDX, IDC_MYCHECKBOX, m_pSet->m_bColumn2, m_pSet);
+	// 有关详细信息，请参阅 MSDN 和 ODBC 示例
+	DDX_Text(pDX, IDC_EDIT1, m_pSet->m_1);
+}
+
+BOOL C综合实验三View::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CRecordView::PreCreateWindow(cs);
+}
+
+void C综合实验三View::OnInitialUpdate()
+{
+	m_pSet = &GetDocument()->m_综合实验三Set;
+	CRecordView::OnInitialUpdate();
+
+}
+
+
+// C综合实验三View 诊断
+
+#ifdef _DEBUG
+void C综合实验三View::AssertValid() const
+{
+	CRecordView::AssertValid();
+}
+
+void C综合实验三View::Dump(CDumpContext& dc) const
+{
+	CRecordView::Dump(dc);
+}
+
+C综合实验三Doc* C综合实验三View::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(C综合实验三Doc)));
+	return (C综合实验三Doc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// C综合实验三View 数据库支持
+CRecordset* C综合实验三View::OnGetRecordset()
+{
+	return m_pSet;
+}
+
+
+
+// C综合实验三View 消息处理程序
+
+
+void C综合实验三View::OnBnClickedButton3()
+{
+	// TODO: 在此添加控件通知处理程序代码
+	m_pSet->Delete();
+	m_pSet->MovePrev();
+	if (m_pSet->IsBOF())
+		m_pSet->MoveFirst();
+	UpdateData(false);
+}
+
+
+void C综合实验三View::OnBnClickedButton1()
+{
+	// TODO: 在此添加控件通知处理程序代码
+	ADD_DLG adddlg;
+	int r = adddlg.DoModal();
+	if (r == IDOK) {
+		long ii = adddlg.l;
+		m_pSet->AddNew();
+		m_pSet->m_1 = ii;
+		m_pSet->Update();
+		UpdateData(false);
+	}
+
+}
+
+
+void C综合实验三View::OnBnClickedButton2()
+{
+	// TODO: 在此添加控件通知处理程序代码
+	ADD_DLG adddlg;
+	int r = adddlg.DoModal();
+	if (r == IDOK) {
+		long ii = adddlg.l;
+		m_pSet->Edit();
+		m_pSet->m_1 = ii;
+		m_pSet->Update();
+		UpdateData(false);
+	}
+}
---
+++ 3/综合实验三/综合实验三/综合实验三View.h
@@ -0,0 +1,60 @@
+
+// 综合实验三View.h : C综合实验三View 类的接口
+//
+
+#pragma once
+
+class C综合实验三Set;
+
+class C综合实验三View : public CRecordView
+{
+protected: // 仅从序列化创建
+	C综合实验三View();
+	DECLARE_DYNCREATE(C综合实验三View)
+
+public:
+#ifdef AFX_DESIGN_TIME
+	enum{ IDD = IDD_MY_FORM };
+#endif
+	C综合实验三Set* m_pSet;
+
+// 特性
+public:
+	C综合实验三Doc* GetDocument() const;
+
+// 操作
+public:
+
+// 重写
+public:
+	virtual CRecordset* OnGetRecordset();
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+	virtual void OnInitialUpdate(); // 构造后第一次调用
+
+// 实现
+public:
+	virtual ~C综合实验三View();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	DECLARE_MESSAGE_MAP()
+public:
+	long l;
+	afx_msg void OnBnClickedButton3();
+	afx_msg void OnBnClickedButton1();
+	afx_msg void OnBnClickedButton2();
+};
+
+#ifndef _DEBUG  // 综合实验三View.cpp 中的调试版本
+inline C综合实验三Doc* C综合实验三View::GetDocument() const
+   { return reinterpret_cast<C综合实验三Doc*>(m_pDocument); }
+#endif
+
